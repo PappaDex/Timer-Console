1a338b45eaaa3639ae3b29c90f9e7840bef31020
Đurica Dakić <durica.dakic@vuv.hr>
2022-11-29 14:25:08 +0100

    Add project files.

---
+++ Timer Console/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace Timer_Console
+{
+    class Timer
+    {
+        TimerCallback _callback;
+        object _state;
+        int _dueTime;
+        int _period;
+        public Timer(TimerCallback callback,object state,int dueTime,int period)
+        {
+            _callback = callback;
+            _state = state;
+            _dueTime = dueTime;
+            _period = period;
+        }
+        public Timer(TimerCallback callback)
+        {
+            _callback = callback;
+        }
+        public void Change(int due,int inf)
+        {
+            _period = inf;
+            _dueTime = due;
+        }
+    }
+}
